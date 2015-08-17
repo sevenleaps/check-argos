73128642a10bc34b1d23d64456143f63946bf5fd
Conor Fennell <fennell.conor@gmail.com>
2015-08-17 18:45:17 +0100

    Revert "Revert "Added price history route""
    This reverts commit 1cc7077aa91b7316f0738ad8181fdd37e59566da.

--- server/stock/stock_routes.js
+++ server/stock/stock_routes.js
@@ -3,6 +3,8 @@
   var controller = require('./stock_controllers.js');
 
   module.exports = exports = function (router) {
+    router.route('/price/:productId')
+      .get(controller.getPriceHistory);
     router.route('/:storeId/:productId')
       .get(controller.checkStock);
   };
