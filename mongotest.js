6b49654bb72a6da8c606f07dc7de5eeda20b15ba
Conor Fennell <fennell.conor@gmail.com>
2015-10-13 23:02:14 +0100

    push to mongo argos uk

--- mongotest.js
+++ mongotest.js
@@ -1,37 +1,25 @@
 var mongodb = require('mongodb-then');
 var moment = require('moment');
 
-var db = mongodb('sevenleaps:S1lk4dose@ds029803.mongolab.com:29803/checkargos', [
+var db = mongodb('sevenleaps:S1lk4dose@ds037244.mongolab.com:37244/stockspy', [
   'product','price'
 ]);
 //Converter Class
-// var fs = require('fs');
-// var Converter = require('csvtojson').Converter;
-// var fileStream = fs.createReadStream('/Users/conor/repos/check-argos/f2.txt');
-// //new converter instance
-// var converter = new Converter({constructResult:true});
-// //end_parsed will be emitted once parsing finished
-// converter.on('end_parsed', function (prices) {
-//   //  console.log(jsonObj[0]); //here is your result json object
-//   // var hashMap = {};
-//   // jsonObj.forEach(function (item) {
-//   //   hashMap[item.SKU] = hashMap[item.SKU] === undefined ? 1 : hashMap[item.SKU] + 1;
-//   // })
-//   //
-//   // var filtered = jsonObj.filter(function (item) {
-//   //   console.log( item.SKU + ' ' + hashMap[item.SKU]);
-//   //   hashMap[item.SKU] > 1
-//   // })
-//   // console.log(filtered[0]);
-//   // console.log(filtered.length)
-//
-//   insertTwenty(prices).catch(function (err) {
-//     console.log(err);
-//   });
-//
-// });
-// //read from file
-// fileStream.pipe(converter);
+var fs = require('fs');
+var Converter = require('csvtojson').Converter;
+var fileStream = fs.createReadStream('/Users/conor/repos/check-argos/argos_uk_365.csv');
+//new converter instance
+var converter = new Converter({constructResult:true});
+//end_parsed will be emitted once parsing finished
+converter.on('end_parsed', function (prices) {
+
+  insertTwenty(prices).catch(function (err) {
+    console.log(err);
+  });
+
+});
+//read from file
+fileStream.pipe(converter);
 
 function getTwenty(array) {
   var twenty = [];
@@ -57,9 +45,9 @@ function insertTwenty(prices) {
 
 var productId = "2627443"//productInfoJson.productId.replace('/', '');
 
-db.price.find({'productId': Number.parseInt(productId)}).sort({'$natural': -1}).limit(1).then(function (array) {
-  console.log(array);
-})
+// db.price.find({'productId': Number.parseInt(productId)}).sort({'$natural': -1}).limit(1).then(function (array) {
+//   console.log(array);
+// })
 
 // var productPrice = productInfoJson.price.replace('.', '');
 //
