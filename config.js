f80e7d5ba9e1606ce1893826fdf231400b71a76a
conorfennell <fennell.conor@gmail.com>
2020-11-08 12:03:18 +0000

    remove exiting on uncaught error

--- config.js
+++ config.js
@@ -63,12 +63,5 @@
   // Catch the uncaught errors that weren't wrapped in a domain or try catch statement.
   function onUncaughtException(err) {
     console.log('Uncaught exception:', err);
-
-    // delay exit to allow error to be reported to Rollbar
-    setTimeout(process.exit.bind(null, 1), 2000);
-
-    // avoid reporting more errors while the process is waiting to be killed
-    process.removeListener('unhandledRejection', onUncaughtException);
-    process.removeListener('uncaughtException', onUncaughtException);
   }
 })();
